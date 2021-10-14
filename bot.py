fabff635109e0f094142c4194f42f234b41b9f68
BuildTools <unconfigured@null.spigotmc.org>
2021-10-15 00:13:35 +0200

    Fixed and expanded bot by printing received messages.

--- bot.py
+++ bot.py
@@ -2,13 +2,20 @@ import discord
 from discord.ext import commands
 from pathlib import Path
 
-token = Path.read_text('token.txt')
+token = Path('token.txt').read_text()
 token = token.replace('\n', '')
 
 client = commands.Bot(command_prefix='#')
 
 @client.event
 async def on_ready():
-    print('bot is ready as {0.user}', client)
+    print('bot is ready as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    else:
+        print('Received message from {0.author}: "{0.content}"'.format(message))
 
 client.run(token)
