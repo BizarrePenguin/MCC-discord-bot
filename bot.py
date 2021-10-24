128e102418ed93f699c143eeb002c9a4ee431c39
BizarrePenguin <19168395+BizarrePenguin@users.noreply.github.com>
2021-10-24 21:15:39 +0200

    Sophisticated beginning

--- bot.py
+++ bot.py
@@ -1,22 +1,46 @@
 import discord
 from discord.ext import commands
+from typing import Union
 from pathlib import Path
 
-token = Path('token.txt').read_text()
+# Get root discord-bot project folder
+DIR = Path(__file__).parent
+
+# Get bot oken from file
+token_path = DIR / 'token.txt'
+token = token_path.read_text()
 token = token.replace('\n', '')
 
-client = commands.Bot(command_prefix='#')
+# Define checks
+def is_dm(ctx: Union[discord.Message, commands.Context]):
+    return (ctx.channel.type == discord.ChannelType.private)
 
-@client.event
-async def on_ready():
-    print('bot is ready as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# Define command prefix
+def command_prefix(bot, message):
+    if is_dm(message):
+        return ''
     else:
-        print('Received message from {0.author}: "{0.content}"'.format(message))
-        await message.channel.send('Hello there! I am currently a baby bot. Please do not expect good answers from me yet.')
+        return '$'
 
-client.run(token)
+# Get commands-bot instance
+bot = commands.Bot(command_prefix=command_prefix)
+
+# Set global checks
+@bot.check(is_dm)
+
+# Set bot events
+@bot.event
+async def on_ready():
+    print('bot is ready as {0.user}'.format(bot))
+
+# Set bot commands
+@bot.command()
+async def test(ctx):
+    await ctx.send('Hello there. You found the test command 👌🏼')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello there! I am currently a baby bot. Please do not expect good answers from me yet.')
+
+# Run bot
+bot.run(token)
